3b5c3e4dc142fafb1331450697699e506ab5120c
Mary Lofton <melofton@vt.edu>
2024-02-23 13:10:56 -0500

    batch run files

---
+++ run_r.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd FCRE-forecast-code
+/usr/local/bin/r workflows/ler_ms/01_combined_paper_workflow.R
---
+++ submit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -J flare_ler
+#SBATCH --account=flare
+#SBATCH --partition=normal_q
+#SBATCH --nodes=1 --ntasks-per-node=1 --cpus-per-task=8 # this requests 1 node, 1 core.
+#SBATCH --time=0-48:00:00 # 10 minutes
+
+module load containers/singularity
+singularity exec --bind=/home/freyao/FCRE-forecast-code:/home/rstudio/FCRE-forecast-code /projects/rqthomas_lab/eco4cast_containers/rocker-neon4cast_4.2.1.sif /bin/bash run_r.sh
